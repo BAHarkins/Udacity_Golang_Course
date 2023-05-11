e437c9bcfe21f7e000f0d928d1f0222eeab1b61f
BAHarkins <121116786+BAHarkins@users.noreply.github.com>
2023-05-11 14:58:50 -0400

    Variables Lesson

---
+++ Go Fundamentals/Variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var Language = "Go"
+	const released = 2009
+	isAProgrammingLanguage := true
+	fmt.Println(Language, released, isAProgrammingLanguage)
+}
